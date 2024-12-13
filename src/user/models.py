7cb96a563e6ac0110d1de10671bcbe4e29d8c177
Tauraih <tauraihv@gmail.com>
2024-12-13 21:59:31 +0200

    add user app

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,65 @@
+   
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for user with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email and not validate_email(email):
+            raise ValidationError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_user(email, password, **extra_fields)
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = ShortUUIDField(primary_key=True)
+    username = None
+    firstname = models.CharField(max_length=64, null=True, blank=True)
+    lastname = models.CharField(max_length=64, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    is_deleted = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = 'user'
+        default_related_name = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f"{self.firstname} | {self.email}"
