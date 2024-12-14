7aad7c5ffdcbc5c4e1207062759e50745bfb805a
Tauraih <tauraihv@gmail.com>
2024-12-14 20:01:41 +0200

    update static dirs

--- src/app/settings/__init__.py
+++ src/app/settings/__init__.py
@@ -107,7 +107,7 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "mystaticfiles"
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
