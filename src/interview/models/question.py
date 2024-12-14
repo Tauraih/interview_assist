f5a187213fbc8ba9ebe76d2e65b95681f29a4e68
Tauraih <tauraihv@gmail.com>
2024-12-14 20:04:16 +0200

    clean qsn models

--- src/interview/models/question.py
+++ src/interview/models/question.py
@@ -12,7 +12,7 @@ class Question(models.Model):
     """The static Question Bank serves as a fallback or seed for general questions."""
 
     id = ShortUUIDField(primary_key=True)
-    job_type = models.ManyToManyField("interview.JobType", related_name="qsn_job_types", null=True, blank=True)
+    job_type = models.ManyToManyField("interview.JobType", related_name="qsn_job_types", blank=True)
     skill = models.JSONField(null=True, blank=True)
     difficult_level = models.CharField(max_length=32, choices=Difficutly.choices, default=Difficutly.EASY)
     text = models.CharField(max_length=1024)
@@ -33,7 +33,7 @@ class UserQuestion(models.Model):
 
     id = ShortUUIDField(primary_key=True)
     user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="user_questions")
-    job_type = models.ManyToManyField("interview.JobType", related_name="user_qsn_job_types", null=True, blank=True)
+    job_type = models.ManyToManyField("interview.JobType", related_name="user_qsn_job_types", blank=True)
     skill = models.JSONField(null=True, blank=True)
     text = models.CharField(max_length=1024)
     answer_guidelines = models.CharField(max_length=500, null=True, blank=True)
